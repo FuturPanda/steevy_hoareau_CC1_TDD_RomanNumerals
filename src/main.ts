35a1a31a3accb707916d761a3644c959c5d354aa
Hoareau.Steevy <shoareau@dev-id.fr>
2025-10-30 16:14:16 +0100

    add: test and minimal function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,44 @@
 export function toRoman(number: number): string {
-  return '';
+  if (number < 1 || number > 3999) throw new Error('number outside parameters');
+  const digitsArray = number.toString().split('');
+  const arrayLength = digitsArray.length;
+  const romanLetters = digitsArray.map((digit, index) => {
+    const position = arrayLength - index;
+    switch (digit) {
+      case '1': {
+        if (position == 1) return 'I';
+        if (position == 2) return 'X';
+        if (position == 3) return 'C';
+        if (position == 4) return 'M';
+        break;
+      }
+      case '2':
+      case '3': {
+        const multiplier = digit === '2' ? 2 : 3;
+        if (position == 1) return 'I'.repeat(multiplier);
+        if (position == 2) return 'X'.repeat(multiplier);
+        if (position == 3) return 'C'.repeat(multiplier);
+        if (position == 4) return 'M'.repeat(multiplier);
+        break;
+      }
+      case '5': {
+        if (position == 1) return 'V';
+        if (position == 2) return 'L';
+        if (position == 3) return 'D';
+        break;
+      }
+      case '6':
+      case '7':
+      case '8': {
+        const extra = parseInt(digit) - 5;
+        if (position == 1) return 'V' + 'I'.repeat(extra);
+        if (position == 2) return 'L' + 'X'.repeat(extra);
+        if (position == 3) return 'D' + 'C'.repeat(extra);
+        break;
+      }
+      default:
+        return '';
+    }
+  });
+  return romanLetters.join('');
 }
---
+++ test/converter.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, test } from 'vitest';
+import { toRoman } from '../src/main';
+
+const MINUS_HUNDRED = -100;
+const FOUR_THOUSAND = 4000;
+
+const ZERO = 0;
+const ONE = 1;
+const FIVE = 5;
+const TEN = 10;
+const FIFTY = 50;
+const ONE_HUNDRED = 100;
+const FIVE_HUNDRED = 500;
+const ONE_THOUSAND = 1000;
+
+const I = 'I';
+const V = 'V';
+const X = 'X';
+const L = 'L';
+const C = 'C';
+const D = 'D';
+const M = 'M';
+
+describe('Global rules', () => {
+  test('Should throw if number is 0', () => {
+    const functionThatShouldThow = () => toRoman(ZERO);
+    expect(functionThatShouldThow).toThrowError();
+  });
+
+  test('Should throw if number is negative', () => {
+    const functionThatShouldThow = () => toRoman(MINUS_HUNDRED);
+    expect(functionThatShouldThow).toThrowError();
+  });
+
+  test('Should throw if number is superior or egal to four thousand', () => {
+    const functionThatShouldThow = () => toRoman(FOUR_THOUSAND);
+    expect(functionThatShouldThow).toThrowError();
+  });
+});
+
+describe('Number converter', () => {
+  test('should return the corresponding roman letter when entering the corresponding number', () => {
+    const inputNumbers = [ONE, FIVE, TEN, FIFTY, ONE_HUNDRED, FIVE_HUNDRED, ONE_THOUSAND];
+    const expectedResult = [I, V, X, L, C, D, M];
+    const convertedNumbers = inputNumbers.map(toRoman);
+    expect(convertedNumbers).toStrictEqual(expectedResult);
+  });
+
+  test('should return combined roman letters for a given arabic number', () => {
+    const inputNumbers = [133, 256, 3000];
+    const expectedResult = ['CXXXIII', 'CCLVI', 'MMM'];
+    const convertedNumbers = inputNumbers.map(toRoman);
+    expect(convertedNumbers).toStrictEqual(expectedResult);
+  });
+});
